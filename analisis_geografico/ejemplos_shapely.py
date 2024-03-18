9f6901ef7ec36edec38e85dabefcf88e038dedc0
Oscar Martínez <oscaraen@hotmail.com>
2024-03-18 08:52:03 -0500

    adicion (Cuadernos de ciencia de datos)
    Se crea el repositorio y se añaden cuadernos de ciencia de datos, análisis exploratorio de los datos, regresión, clasificación (incompleto) y análisis de datos geoespaciales.

---
+++ analisis_geografico/ejemplos_shapely.py
@@ -0,0 +1,13 @@
+from shapely.geometry import Point, LineString, Polygon
+
+
+point = Point(1,1)
+
+point = Point(0,0)
+point = Point(3,1)
+point = Point(0,3)
+
+line1 = LineString([point1, point2, point3])
+
+
+
